758ad2c4809cfaa341bb46755f027162ab600eac
Gabriela Kotseva <gkotseva98@gmail.com>
2024-03-27 13:31:44 +0200

    Home page created

---
+++ client/src/components/home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.modules.css'
+
+export function Home () {
+    return (
+        <>
+            <div className='container'>
+                <img src='gallery1.jpg'></img>
+                <img src='gallery2.jpg'></img>
+                <img src='gallery3.jpg'></img>
+            </div>
+        </>
+    )
+}
