0efde1ca1325a6df799d37679923b39e2a0c7aca
Gabriela Kotseva <gkotseva98@gmail.com>
2024-03-27 13:31:17 +0200

    Initialize router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,16 @@
+import './index.css'
+import { Navigation } from "./components/navigation/Navigation"
+import {BrowserRouter} from 'react-router-dom'
+import { Home } from './components/home/Home'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Navigation />
+      <Home />
+    </BrowserRouter>
     </>
   )
 }
