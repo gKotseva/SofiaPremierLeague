339b58334aa3705da9e4718d86972f94821aaf2b
Gabriela Kotseva <gkotseva98@gmail.com>
2024-03-28 15:44:38 +0200

    New component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import './index.css'
 import { Navigation } from "./components/navigation/Navigation"
 import {BrowserRouter} from 'react-router-dom'
 import { Home } from './components/home/Home'
+import { TableStandings } from './components/table/TableStandings'
 
 function App() {
 
@@ -10,6 +11,7 @@ function App() {
     <BrowserRouter>
       <Navigation />
       <Home />
+      <TableStandings />
     </BrowserRouter>
     </>
   )
