bc180c7fa5489c278f9e90bbb7baf3b5a62e746a
Gabriela Kotseva <gkotseva98@gmail.com>
2024-03-29 18:01:15 +0200

    Added new component to the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,8 @@ import './index.css'
 import { Navigation } from "./components/navigation/Navigation"
 import {BrowserRouter} from 'react-router-dom'
 import { Home } from './components/home/Home'
-import { TableStandings } from './components/table/TableStandings'
+import { TableStandings } from './components/tables/TableStandings'
+import { TableResults } from './components/tables/tableResults/TableResults'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Navigation />
       <Home />
       <TableStandings />
+      <TableResults />
     </BrowserRouter>
     </>
   )
