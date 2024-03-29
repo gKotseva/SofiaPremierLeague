bed653cad4b660662d5bbe458a487a8ac37e1649
Gabriela Kotseva <gkotseva98@gmail.com>
2024-03-29 18:01:04 +0200

    New component created

---
+++ client/src/components/tables/tableResults/TableResults.jsx
@@ -0,0 +1,72 @@
+import './TableResults.modules.css'
+
+export function TableResults () {
+    return (
+        <>
+                <div className='fixtures'>
+            <table>
+                <thead>
+                    <tr>
+                        <th>Дата</th>
+                        <th>Домакин</th>
+                        <th>Час</th>
+                        <th>Гост</th>
+                        <th>Терен</th>
+                        <th>Ден</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                    <tr>
+                        <td>27.01.2021</td>
+                        <td>Домакин 1</td>
+                        <td>19:30</td>
+                        <td>Гост 1</td>
+                        <td>Терен 1</td>
+                        <td>Неделя</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+        </>
+    )
+}
