5183cdc49d4affdb6fcf79e7c4fcf66396552d00
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-01 16:08:45 +0300

    New component structure created

---
+++ client/src/components/contact/Contact.jsx
@@ -0,0 +1,13 @@
+import './Contact.modules.css'
+
+export function Contact () {
+    return (
+        <>
+        <div className='contact'>
+            <h1>Записване</h1>
+            <p>Изпратете попълнената бланка заедно с логото на отбора на мейл адреса ни:</p>
+            <span>spl.team.info@gmail.com</span>
+        </div>
+        </>
+    )
+}
