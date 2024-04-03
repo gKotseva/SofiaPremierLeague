98342d1ef347a76db6abed4e2dfa1966bea9c503
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-03 15:12:38 +0300

    Added router

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,26 +1,19 @@
 import './index.css'
-import { Navigation } from "./components/navigation/Navigation"
-import {BrowserRouter} from 'react-router-dom'
-import { Home } from './components/home/Home'
-import { TableStandings } from './components/tables/tableStandings/TableStandings'
-import { TableFixtures } from './components/tables/tableFixtures/TableFixtures'
-import { TableResults } from './components/tables/tableResults/TableResults'
-import { Contact } from './components/contact/Contact'
-import { HallOfFame } from './components/hallOfFame/HallOfFame'
+
+import {Navigation} from './components/navigation/Navigation.jsx'
+import { Route, Routes } from 'react-router-dom'
+import { PATH } from './paths.js'
+import { HomeMain } from './components/home/HomeMain.jsx'
+
 
 function App() {
 
   return (
     <>
-    <BrowserRouter>
-      <Navigation />
-      <HallOfFame />
-      <Contact />
-      <Home />
-      <TableStandings />
-      <TableFixtures />
-      <TableResults />
-    </BrowserRouter>
+    <Navigation />
+    <Routes>
+      <Route path={PATH.home} element={<HomeMain />}></Route>
+    </Routes>
     </>
   )
 }
--- client/src/components/navigation/Navigation.jsx
+++ client/src/components/navigation/Navigation.jsx
@@ -10,61 +10,61 @@ export function Navigation() {
             <div className='menu-container'>
                 <ul>
                     <li className='menu-item'><Link to="/">Начало</Link></li>
-                    <li className='menu-item'><Link to="/">Новини <span>&#11167;</span></Link>
+                    <li className='menu-item'><Link to="/novini">Новини <span>&#11167;</span></Link>
                         <ul className='dropdown'>
-                            <li><Link to="/">Наказания</Link></li>
-                            <li><Link to="/">Миксзона</Link></li>
-                            <li><Link to="/">Играчи със спрени състезателни права</Link></li>
+                            <li><Link to="/nakazaniq">Наказания</Link></li>
+                            <li><Link to="/mix-zona">Миксзона</Link></li>
+                            <li><Link to="/prava">Играчи със спрени състезателни права</Link></li>
                         </ul>
                     </li>
-                    <li className='menu-item'><Link to="/">Класации <span>&#11167;</span></Link>
+                    <li className='menu-item'><Link to="/klasatsii">Класации <span>&#11167;</span></Link>
                         <ul className='dropdown'>
-                            <li><Link to="/">Select - гол на месеца</Link></li>
-                            <li><Link to="/">Kerelski - играч на седмицата</Link></li>
-                            <li><Link to="/">GripSocks BG - играч на седмицата</Link></li>
-                            <li><Link to="/">VR7 Services - играч на седмицата</Link></li>
-                            <li><Link to="/">Barber Ship Marty - гол на седмицата</Link></li>
-                            <li><Link to="/">Cair - феърплей</Link></li>
+                            <li><Link to="/select-gol-mesets">Select - гол на месеца</Link></li>
+                            <li><Link to="/kerelski-igrach-sedmitsa">Kerelski - играч на седмицата</Link></li>
+                            <li><Link to="/gripsocks-igrach-sedmitsa">GripSocks BG - играч на седмицата</Link></li>
+                            <li><Link to="/vr7-igrach-sedmitsa">VR7 Services - играч на седмицата</Link></li>
+                            <li><Link to="/barber-gol-sedmitsa">Barber Ship Marty - гол на седмицата</Link></li>
+                            <li><Link to="/cairo-fareplay">Cairo - феърплей</Link></li>
                         </ul>
                     </li>
-                    <li className='menu-item'><Link to="/">Състезания <span>&#11167;</span></Link>
+                    <li className='menu-item'><Link to="/sustezaniq">Състезания <span>&#11167;</span></Link>
                         <ul className='dropdown'>
-                            <li><Link to="/">Зимна купа SPL MK Football &#11166;</Link>
+                            <li><Link to="/zimna-kupa">Зимна купа SPL MK Football &#11166;</Link>
                                 <ul className='dropdown-second'>
-                                    <li><Link to="/">Класиране, програма, резултати – 2024</Link></li>
-                                    <li><Link to="/">Схема на турнира - 2024</Link></li>
-                                    <li><Link to="/">Статистика</Link></li>
+                                    <li><Link to="/klasirane-programa-resultati">Класиране, програма, резултати – 2024</Link></li>
+                                    <li><Link to="/shema-turnir">Схема на турнира - 2024</Link></li>
+                                    <li><Link to="/zimna-kupa-statistica">Статистика</Link></li>
                                 </ul>
                             </li>
-                            <li><Link to="/">Select Суперлига</Link></li>
-                            <li><Link to="/">SPL Malus-Sport</Link></li>
-                            <li><Link to="/">SPL 1 Cairo</Link></li>
-                            <li><Link to="/">SPL 2 Flame</Link></li>
-                            <li><Link to="/">SPL 3 Arabesk</Link></li>
-                            <li><Link to="/">SPL 4</Link></li>
-                            <li><Link to="/">SPL 5</Link></li>
+                            <li><Link to="/superliga">Select Суперлига</Link></li>
+                            <li><Link to="/malus">SPL Malus-Sport</Link></li>
+                            <li><Link to="/cairo">SPL 1 Cairo</Link></li>
+                            <li><Link to="/flame">SPL 2 Flame</Link></li>
+                            <li><Link to="/arabesk">SPL 3 Arabesk</Link></li>
+                            <li><Link to="/spl4">SPL 4</Link></li>
+                            <li><Link to="/spl5">SPL 5</Link></li>
                         </ul>
                     </li>
-                    <li className='menu-item'><Link to="/">Статистика <span>&#11167;</span></Link>
+                    <li className='menu-item'><Link to="/statistika">Статистика <span>&#11167;</span></Link>
                         <ul className='dropdown'>
-                            <li><Link to="/">Select Суперлига</Link></li>
-                            <li><Link to="/">SPL Malus-Sport</Link></li>
-                            <li><Link to="/">SPL 1 Cairo</Link></li>
-                            <li><Link to="/">SPL 2 Flame</Link></li>
-                            <li><Link to="/">SPL 3 Arabesk</Link></li>
-                            <li><Link to="/">SPL 4</Link></li>
-                            <li><Link to="/">SPL 5</Link></li>
-                            <li><Link to="/">SPL Север</Link></li>
-                            <li><Link to="/">SPL Изток</Link></li>
-                            <li><Link to="/">Malus Summer Football Cup - 2020</Link></li>
+                            <li><Link to="/statistica-superliga">Select Суперлига</Link></li>
+                            <li><Link to="/statistica-malus">SPL Malus-Sport</Link></li>
+                            <li><Link to="/statistica-cairo">SPL 1 Cairo</Link></li>
+                            <li><Link to="/statistica-flame">SPL 2 Flame</Link></li>
+                            <li><Link to="/statistica-arabesk">SPL 3 Arabesk</Link></li>
+                            <li><Link to="/statistica-spl4">SPL 4</Link></li>
+                            <li><Link to="/statistica-spl5">SPL 5</Link></li>
+                            <li><Link to="/statistica-sever">SPL Север</Link></li>
+                            <li><Link to="/statistica-iztok">SPL Изток</Link></li>
+                            <li><Link to="/statistica-summer">Malus Summer Football Cup - 2020</Link></li>
                         </ul>
                     </li>
-                    <li className='menu-item'><Link to="/">Hall of Fame</Link></li>
-                    <li className='menu-item'><Link to="/">За нас <span>&#11167;</span></Link>
+                    <li className='menu-item'><Link to="/hall-of-fame">Hall of Fame</Link></li>
+                    <li className='menu-item'><Link to="/za-nas">За нас <span>&#11167;</span></Link>
                         <ul className='dropdown'>
-                            <li><Link to="/">Записване</Link></li>
-                            <li><Link to="/">Правила</Link></li>
-                            <li><Link to="/">Контакти</Link></li>
+                            <li><Link to="/zapisvane">Записване</Link></li>
+                            <li><Link to="/pravila">Правила</Link></li>
+                            <li><Link to="/kontakti">Контакти</Link></li>
                         </ul>
                     </li>
                 </ul>
