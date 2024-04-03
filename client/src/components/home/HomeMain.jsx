88fb1f8bd95946cca2797be424bd95f3c2433a14
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-03 15:11:22 +0300

    New component created

---
+++ client/src/components/home/HomeMain.jsx
@@ -0,0 +1,15 @@
+import { Home } from "./HomeImages/HomeImages";
+import { TableFixtures } from "./Tables/tableFixtures/TableFixtures";
+import { TableResults } from "./Tables/tableResults/TableResults";
+import { TableStandings } from "./Tables/tableStandings/TableStandings";
+
+export function HomeMain () {
+    return (
+        <>
+        <Home />
+        <TableStandings />
+        <TableFixtures />
+        <TableResults />
+        </>
+    )
+}
