cb1d9dfe1650adec894e9d5975031f0d519dfa0d
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-18 14:49:27 +0300

    New component added

---
+++ client/src/components/sustezaniya/SPLMalus.jsx
@@ -0,0 +1,9 @@
+import { TableStandings } from "../home/Tables/tableStandings/TableStandings";
+
+export function SPLMalus () {
+    return (
+        <>
+            <TableStandings />
+        </>
+    )
+}
