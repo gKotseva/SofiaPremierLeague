ea3cb1d27b44f85f2cf0003be2af71ab6536ff68
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-18 14:49:38 +0300

    New component added

---
+++ client/src/components/sustezaniya/SPL2Flame.jsx
@@ -0,0 +1,9 @@
+import { TableStandings } from "../home/Tables/tableStandings/TableStandings";
+
+export function SPL2Flame () {
+    return (
+        <>
+            <TableStandings />
+        </>
+    )
+}
