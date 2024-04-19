ad4474412890c13228849b2fb7f170982494e8b8
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-19 14:48:52 +0300

    New component added

---
+++ client/src/components/statistika/pages/ShemaKupa.jsx
@@ -0,0 +1,8 @@
+export function ShemaKupa(){
+    return (
+        <>
+            <img src="./GOLYAMA-zimna-kupa-shablon-2-GOTOVA-CHETART-FINAL.jpg"></img>
+            <img src="./MALKA-zimna-kupa-shablon-1-GOTOVA1-CHETART-FINAL.jpg"></img>
+        </>
+    )
+}
