8acbf592b33766f06b7b7e3f1c3dd2043c8cf4e9
Gabriela Kotseva <gkotseva98@gmail.com>
2024-04-19 14:39:19 +0300

    New components created

---
+++ client/src/components/statistika/pages/SPLIztok.jsx
@@ -0,0 +1,13 @@
+import { Asistencii } from "../tables/Asistencii";
+import { Golmaistori } from "../tables/Golmaistori";
+import { Reiting } from "../tables/Reiting";
+
+export function SPLIztok () {
+    return (
+        <>
+            <Reiting />
+            <Asistencii />
+            <Golmaistori />
+        </>
+    )
+}
