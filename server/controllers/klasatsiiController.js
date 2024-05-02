7bdefb1d09ec86932e36f70cb1f7f09747d6591f
Gabriela Kotseva <gkotseva98@gmail.com>
2024-05-02 17:11:45 +0300

    Adding controller

---
+++ server/controllers/klasatsiiController.js
@@ -0,0 +1,16 @@
+const router = require('express').Router();
+const db = require('../db');
+
+router.get('/api/klasatsii', async (req, res) => {
+    try {
+        const sqlQuery = 'SELECT * FROM awards_arabesk';
+        const results = await db.executeQuery(sqlQuery);
+
+        res.json(results);
+    } catch (error) {
+        console.error('Error executing query:', error);
+        res.status(500).json({ error: 'Internal server error' });
+    }
+});
+
+module.exports = router;
