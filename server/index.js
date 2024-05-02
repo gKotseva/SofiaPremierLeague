f56d194ed0f658977c37db335fcf1f809751a244
Gabriela Kotseva <gkotseva98@gmail.com>
2024-05-02 17:11:36 +0300

    Adding routers and db connection

--- server/index.js
+++ server/index.js
@@ -1,5 +1,11 @@
 const express = require('express')
+const routes = require('./router')
 
 const app = express()
-const PORT = 8888
+const PORT = 9999
+
+app.use(express.json())
+app.use('/', routes)
+
+app.listen(PORT, console.log(`Server is listening on port ${PORT}.`))
 
