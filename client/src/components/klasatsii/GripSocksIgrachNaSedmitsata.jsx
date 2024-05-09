b791375347ae2e744157a9d20c84e031e5682478
Gabriela Kotseva <gkotseva98@gmail.com>
2024-05-09 14:04:30 +0300

    Updated component to pull information from database

--- client/src/components/klasatsii/GripSocksIgrachNaSedmitsata.jsx
+++ client/src/components/klasatsii/GripSocksIgrachNaSedmitsata.jsx
@@ -1,10 +1,39 @@
+import './styles/styles.css'
+
+import { useEffect, useState } from "react"
+import * as service from '../../services/klasatsiiService'
+
 export function GripSocksIgrachNaSedmitsata () {
+
+    const [awards, setAwards] = useState([])
+
+    useEffect(() => {
+        const fetchAwards = async() => {
+            try {
+                const response = await service.getGripSocks()
+                setAwards(response)
+            } catch (error) {
+                console.error('Error fetching awards: ', error)
+            }
+        }
+
+        fetchAwards()
+    }, [])
+
     return (
         <>
+        <div className='award-heading'>
             <h1>GRIPSOCKS BG ИГРАЧ НА СЕДМИЦАТА</h1>
             <p>Всяка седмица най-добре представилият се играч от дивизиите SPL FLAME, SPL3 или SPL4 ще получава специална награда от нашите нови партньори – Gripsocks BG – най-удобните чорапи на пазара! Не пропускайте да си поръчате!</p>
-            <h5>Име играч</h5>
-            <img></img>
+        </div>
+        <div className='award-container'>
+            {awards.map((award) => 
+            <div className='card' key={award.id}>
+                <h5>{award.name}</h5>
+                <img src={award.image}/>
+            </div>
+        )}
+        </div>
         </>
     )
 }
--- client/src/components/klasatsii/KerelskiIgrachNaSedmitsata.jsx
+++ client/src/components/klasatsii/KerelskiIgrachNaSedmitsata.jsx
@@ -1,11 +1,40 @@
+import './styles/styles.css'
+
+import { useEffect, useState } from "react"
+import * as service from '../../services/klasatsiiService'
+
 export function KerelskiIgrachNaSedmitsata () {
+    const [awards, setAwards] = useState([])
+
+    useEffect(() => {
+        const fetchAwards = async() => {
+            try {
+                const response = await service.getKerelski()
+                setAwards(response)
+            } catch (error) {
+                console.error('Error fetching awards: ', error)
+            }
+        }
+
+        fetchAwards()
+    }, [])
+
+
     return (
         <>
+        <div className='award-heading'>
             <h1>KERELSKI ИГРАЧ НА СЕДМИЦАТА</h1>
             <p>Всяка седмица от KERELSKI BARBER & BEAUTY дават награда безплатна подстрежка за най-добре представилият се играч в дивизиите SELECT СУПЕРЛИГА, SPL MALUS-SPORT И SPL CAIRO! Наградата ще бъде присъждана по мнение на организаторите след всеки изигран кръг, където най-добре представилият се футболист ще бъде отличен.</p>
             <p>Всяка седмица победителят ще получава безплатно посещение в най-добрия салон Kerelski Barber & Beauty . Той се намира в кв. „Манастирски ливади“ на ул. Пирин 83. Ваучерът ще важи 3 месеца от получаването!</p>
-            <h5>Име играч</h5>
-            <img></img>
+        </div>
+        <div className='award-container'>
+            {awards.map((award) => 
+            <div className='card' key={award.id}>
+                <h5>{award.name}</h5>
+                <img src={award.image}/>
+            </div>
+            )}
+        </div>
         </>
     )
 }
