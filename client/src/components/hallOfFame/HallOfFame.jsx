f6ee94b2ae45d8af87fa42af0388b2c56f663030
Gabriela Kotseva <gkotseva98@gmail.com>
2024-05-16 14:24:21 +0300

    Requests to the server made

--- client/src/components/hallOfFame/HallOfFame.jsx
+++ client/src/components/hallOfFame/HallOfFame.jsx
@@ -1,6 +1,39 @@
+import { useEffect, useState } from 'react'
 import './HallOfFame.modules.css'
+import * as service from '../../services/klasatsiiService'
+
 
 export function HallOfFame () {
+    const [award5Years, setAward5Years] = useState([])
+
+    useEffect(() => {
+        const fetchAward5Years = async () => {
+            try {
+                const response = await service.getAward5Years()
+                setAward5Years(response)
+            }
+            catch (error) {
+                console.error('Error fetching awards: ', error)
+            }
+        }
+        fetchAward5Years()
+    }, [])
+
+    const [award200Goals, setAward200Goals] = useState([])
+
+    useEffect(() => {
+        const fetchAward200Goals = async () => {
+            try {
+                const response = await service.getAward200Goals()
+                setAward200Goals(response)
+            }
+            catch (error) {
+                console.error('Error fetching awards: ', error)
+            }
+        }
+        fetchAward200Goals()
+    }, [])
+
     return (
         <>
         <div className='hall-of-fame-table'>
@@ -97,10 +130,23 @@ export function HallOfFame () {
             <p>Емо Стефанов (Titanite Old Stars) – 9 секунда в мача Вълците – Titanite Old Stars (2:2 – 7 юни 2020 година)</p>
             <iframe src="https://www.youtube.com/watch?v=DKaqVCd3SjY"></iframe>
         </div>
-        <div className='200-goals'>
+        <div className='200-goals-container'>
             <h1>Клуб 200 гола</h1>
-            <h4>Играч - име</h4>
-            <img />
+            {award200Goals.map((award) => 
+                <div className='200-goals-card' key={award.id}>
+                    <h4>{award.name}</h4>
+                    <img src={award.image} />
+                </div>
+        )}
+        </div>
+        <div className='5-years-container'>
+            <h1>5 години в SPL</h1>
+            {award5Years.map((award) => 
+                <div className='5-years-card' key={award.id}>
+                    <h4>{award.name}</h4>
+                    <img src={award.image} />
+            </div>
+            )}
         </div>
         </>
     )
