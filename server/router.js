d3c04bab755210cf5ce008902643209a5fcbfb10
Gabriela Kotseva <gkotseva98@gmail.com>
2024-05-16 14:23:59 +0300

    added new controller to the router

--- server/router.js
+++ server/router.js
@@ -1,7 +1,9 @@
 const router = require('express').Router()
 const klasatsiiController = require('./controllers/klasatsiiController.js')
+const hallOfFameController = require('./controllers/hallOfFameController.js')
 
 router.use('/api/klasatsii', klasatsiiController)
+router.use('/api/hall-of-fame', hallOfFameController)
 
 
 module.exports = router
