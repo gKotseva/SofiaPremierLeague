8526f6e2894f5e3187fd9df469338ffe2d4357a3
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-06 18:25:29 +0300

    SQL queries fixed as per the new DB structure

--- server/controllers/hallOfFameController.js
+++ server/controllers/hallOfFameController.js
@@ -4,7 +4,7 @@ const db = require('../db');
 
 router.get('/5years', async (req, res) => {
     try {
-        const sqlQuery = 'SELECT * FROM hallOfFame_5years ORDER BY id desc';
+        const sqlQuery = 'SELECT a.image, p.`name` FROM hallOfFame_5years a INNER JOIN players p ON p.player_id = a.player_id order by a.id desc';
         const results = await db.executeQuery(sqlQuery);
         res.json(results);
     } catch (error) {
@@ -15,7 +15,7 @@ router.get('/5years', async (req, res) => {
 
 router.get('/200goals', async (req, res) => {
     try {
-        const sqlQuery = 'SELECT * FROM hallOfFame_200goals ORDER BY id desc';
+        const sqlQuery = 'SELECT a.image, p.`name` FROM hallOfFame_200goals a INNER JOIN players p ON p.player_id = a.player_id order by a.id desc';
         const results = await db.executeQuery(sqlQuery);
         res.json(results);
     } catch (error) {
@@ -26,7 +26,7 @@ router.get('/200goals', async (req, res) => {
 
 router.get('/champions', async (req, res) => {
     try {
-        const sqlQuery = 'SELECT * FROM hallOfFame_champions'
+        const sqlQuery = 'SELECT l.league_name, s.seasons_name, t.team_name, t.logo_image FROM hallOfFame_champions h INNER JOIN teams t ON t.team_id = h.team_id INNER JOIN leagues l on l.league_id = h.league_id INNER JOIN seasons s on s.season_id=h.season_id';
         const results = await db.executeQuery(sqlQuery)
         res.json(results)
     }
