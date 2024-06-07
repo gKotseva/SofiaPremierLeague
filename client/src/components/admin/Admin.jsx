1117c7bfe5be189359d8af3fa4d02c0187a6471b
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-07 14:59:18 +0300

    New component with structure and styling created

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -1,7 +1,16 @@
+import './Admin.modules.css'
+import { Link } from 'react-router-dom'
+
+
 export function Admin () {
     return (
         <>
-            <h1>Hello</h1>
+            <div className="side-header">
+                <Link><img src="penalty-kick.png"></img></Link>
+                <Link><img src="soccer-player.png"></img></Link>
+                <Link><img src="team.png"></img></Link>
+                <Link><img src="football-club.png"></img></Link>
+            </div>
         </>
     )
 }
