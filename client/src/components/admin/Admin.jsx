b5f91b805670d70d83d00c0efcf33614a31930fa
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-10 14:08:38 +0300

    Minor CSS changes

--- client/src/components/admin/Admin.jsx
+++ client/src/components/admin/Admin.jsx
@@ -6,10 +6,22 @@ export function Admin () {
     return (
         <>
             <div className="side-header">
-                <Link><img src="penalty-kick.png"></img></Link>
-                <Link><img src="soccer-player.png"></img></Link>
-                <Link><img src="team.png"></img></Link>
-                <Link><img src="football-club.png"></img></Link>
+                <div className='overlay'>
+                    <Link><img src="soccer-player.png"></img></Link>
+                    <h1>играчи</h1>
+                </div>
+                <div className='overlay'>
+                    <Link><img src="football-club.png"></img></Link>
+                    <h1>отбори</h1>
+                </div>
+                <div className='overlay'>
+                    <Link><img src="penalty-kick.png"></img></Link>
+                    <h1>мачове</h1>
+                </div>
+                <div className='overlay'>
+                    <Link><img src="team.png"></img></Link>
+                    <h1>персонал</h1>
+                </div>
             </div>
         </>
     )
