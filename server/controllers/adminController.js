2de4da74678d1f716eac6c1af50cabda10a22d9d
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-17 22:27:56 +0300

    DB select change

--- server/controllers/adminController.js
+++ server/controllers/adminController.js
@@ -5,13 +5,13 @@ const db = require('../db');
 router.get('/players', async (req, res) => {
     try {
         const sqlQuery = `
-        SELECT p.player_id, p.name, p.image, p.player_number, pp.position_name, GROUP_CONCAT(distinct t.team_name SEPARATOR ', ') as 'teams' 
+        SELECT p.player_id, p.name, p.image, p.player_number, GROUP_CONCAT(distinct pp.position_name SEPARATOR ', ') as 'position_name', GROUP_CONCAT(distinct t.team_name SEPARATOR ', ') as 'teams' 
         FROM players p
         INNER JOIN player_positions ps ON p.player_id = ps.player_id
         INNER JOIN positions pp ON ps.position_id = pp.position_id
         INNER JOIN player_teams pt ON p.player_id = pt.player_id
         INNER JOIN teams t ON pt.team_id = t.team_id
-        GROUP BY p.player_id, p.name, p.image, p.player_number, pp.position_name
+        GROUP BY p.player_id, p.name, p.image, p.player_number
         ORDER BY p.player_id;
         `;
         const results = await db.executeQuery(sqlQuery);
