49f34fd71c84117ccbfb4b9b132c4c699f7c3504
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-21 17:24:40 +0300

    New service added

--- client/src/services/adminService.js
+++ client/src/services/adminService.js
@@ -1,5 +1,7 @@
 import * as request from '../lib/request';
 
+// GET Requests
+
 export const getPlayers = async () => {
     const result = await request.get('/api/admin/players')
     return result;
@@ -10,17 +12,27 @@ export const getMatches = async () => {
     return result;
 }
 
-export const getTeams= async () => {
+export const getTeams = async () => {
     const result = await request.get('/api/admin/teams')
     return result;
 }
 
-export const getManagers= async () => {
+export const getManagers = async () => {
     const result = await request.get('/api/admin/managers')
     return result;
 }
 
-export const getStaff= async () => {
+export const getStaff = async () => {
     const result = await request.get('/api/admin/staff')
     return result;
 }
+
+// Post requests
+
+export const postStaff = async (formData) => {
+    const result = await request.post('/api/admin/staff', {
+        name: formData.name, 
+    })
+
+    return result;
+};
