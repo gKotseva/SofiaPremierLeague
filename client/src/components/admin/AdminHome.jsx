7937fa03cbc482e5f4f277ac66a59cb062c1bf92
Gabriela Kotseva <gkotseva98@gmail.com>
2024-06-24 23:21:08 +0300

    Added form data to one of the forms and send post request to the server

--- client/src/components/admin/AdminHome.jsx
+++ client/src/components/admin/AdminHome.jsx
@@ -1,78 +1,99 @@
-import { useState } from 'react';
-import './Admin.modules.css'
+import { useEffect, useState } from 'react';
+import './Admin.modules.css';
+import { postStaff } from '../../services/adminService.js';
 
-export function AdminHome () {
+export function AdminHome() {
     const [fileName, setFileName] = useState('Няма избран файл');
+    const [formData, setFormData] = useState({
+        name: '',
+    });
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value,
+        });
+    };
 
     const handleFileChange = (event) => {
-        setFileName(event.target.files[0]?.name || 'Няма избран файл');
+        const file = event.target.files[0];
+        setFileName(file.name || 'Няма избран файл');
     };
+
+    const onSubmit = async (e) => {
+        e.preventDefault()
+
+        const formDataToSend = new FormData();
+        formDataToSend.append('name', formData.name);
+
+        const response = await postStaff(formData.name)
+    }
 
     return (
         <>
-        <div className="form-container">
-            <form className="input-form">
-                <h3>Добави играч</h3>
-                <label>Име<input type="text" /></label>
-                <label>Номер<input type="number" /></label>
-                <div className="file-input-container">
-                    <label className="custom-file-label" htmlFor="file-input">Снимка</label>
-                    <input 
-                        type="file" 
-                        id="file-input" 
-                        className="file-input" 
-                        onChange={handleFileChange}
-                    />
-                    <span className="file-name">{fileName}</span>
-                </div>
-                <button type="submit">Добави</button>
-            </form>
-            <form className="input-form">
-                <h3>Добави отбор</h3>
-                <label>Име<input type="text" /></label>
-                <div className="file-input-container">
-                    <label className="custom-file-label" htmlFor="file-input">Снимка отбор</label>
-                    <input 
-                        type="file" 
-                        id="file-input" 
-                        className="file-input" 
-                        onChange={handleFileChange}
-                    />
-                    <span className="file-name">{fileName}</span>
-                </div>
-                <div className="file-input-container">
-                    <label className="custom-file-label" htmlFor="file-input">Лого</label>
-                    <input 
-                        type="file" 
-                        id="file-input" 
-                        className="file-input" 
-                        onChange={handleFileChange}
-                    />
-                    <span className="file-name">{fileName}</span>
-                </div>
-                <button type="submit">Добави</button>
-            </form>
-            <form className="input-form">
-                <h3>Добави мениджър</h3>
-                <label>Име<input type="text" /></label>
-                <div className="file-input-container">
-                    <label className="custom-file-label" htmlFor="file-input">Снимка</label>
-                    <input 
-                        type="file" 
-                        id="file-input" 
-                        className="file-input" 
-                        onChange={handleFileChange}
-                    />
-                    <span className="file-name">{fileName}</span>
-                </div>
-                <button type="submit">Добави</button>
-            </form>
-            <form className="input-form">
-                <h3>Добави персонал</h3>
-                <label>Име<input type="text" /></label>
-                <button type="submit">Добави</button>
-            </form>
-        </div>
+            <div className="form-container">
+                <form className="input-form">
+                    <h3>Добави играч</h3>
+                    <label>Име<input type="text" name="name"/></label>
+                    <label>Номер<input type="number" /></label>
+                    <div className="file-input-container">
+                        <label className="custom-file-label" htmlFor="file-input">Снимка</label>
+                        <input
+                            type="file"
+                            id="file-input"
+                            className="file-input"
+                        />
+                        <span className="file-name">{fileName}</span>
+                    </div>
+                    <button type="submit">Добави</button>
+                </form>
+                <form className="input-form">
+                    <h3>Добави отбор</h3>
+                    <label>Име<input type="text" /></label>
+                    <div className="file-input-container">
+                        <label className="custom-file-label" htmlFor="team-photo-input">Снимка отбор</label>
+                        <input
+                            type="file"
+                            id="team-photo-input"
+                            className="file-input"
+                            onChange={handleFileChange}
+                        />
+                        <span className="file-name">{fileName}</span>
+                    </div>
+                    <div className="file-input-container">
+                        <label className="custom-file-label" htmlFor="team-logo-input">Лого</label>
+                        <input
+                            type="file"
+                            id="team-logo-input"
+                            className="file-input"
+                            onChange={handleFileChange}
+                        />
+                        <span className="file-name">{fileName}</span>
+                    </div>
+                    <button type="submit">Добави</button>
+                </form>
+                <form className="input-form">
+                    <h3>Добави мениджър</h3>
+                    <label>Име<input type="text" /></label>
+                    <div className="file-input-container">
+                        <label className="custom-file-label" htmlFor="manager-photo-input">Снимка</label>
+                        <input
+                            type="file"
+                            id="manager-photo-input"
+                            className="file-input"
+                            onChange={handleFileChange}
+                        />
+                        <span className="file-name">{fileName}</span>
+                    </div>
+                    <button type="submit">Добави</button>
+                </form>
+                <form className="input-form" onSubmit={onSubmit}>
+                    <h3>Добави персонал</h3>
+                    <label>Име<input type="text" name="name" value={formData.name} onChange={handleChange}/></label>
+                    <button type="submit">Добави</button>
+                </form>
+            </div>
         </>
-    )
+    );
 }
