2b3eb6ee2e77e32ce0214409703dc17891b9a2c6
Gabriela Kotseva <gkotseva98@gmail.com>
2024-07-03 16:03:45 +0300

    New component created

---
+++ client/src/components/admin/AdminHomeStats.jsx
@@ -0,0 +1,35 @@
+export function AdminStats () {
+    return (
+        <>
+            <div className="container-all-stats">
+                <div className="container-stats">
+                <div className="container-stats-image">
+                    <img src="team-color.png"></img>
+                </div>
+                <div className="container-stats-stats">
+                    <p>9999</p>
+                    <p>отбори</p>
+                </div>
+                </div>
+                <div className="container-stats">
+                <div className="container-stats-image">
+                    <img src="football-player.png"></img>
+                </div>
+                <div className="container-stats-stats">
+                    <p>9999</p>
+                    <p>играчи</p>
+                </div>
+                </div>            
+                <div className="container-stats">
+                <div className="container-stats-image">
+                    <img src="football.png"></img>
+                </div>
+                <div className="container-stats-stats">
+                    <p>9999</p>
+                    <p>мачове</p>
+                </div>
+                </div>
+            </div>
+        </>
+    )
+}
