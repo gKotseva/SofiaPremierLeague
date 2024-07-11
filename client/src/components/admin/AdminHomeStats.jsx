cd6f4f41694fa0caf0c78b73254ad100ca1ad39f
Gabriela Kotseva <gkotseva98@gmail.com>
2024-07-11 20:03:09 +0300

    Added new stats feature

--- client/src/components/admin/AdminHomeStats.jsx
+++ client/src/components/admin/AdminHomeStats.jsx
@@ -1,31 +1,31 @@
 import { useEffect, useState } from "react";
 import { getAllStats } from "../../services/adminService";
 
-export function AdminStats () {
+export function AdminStats() {
 
     const [stats, setStats] = useState({
         teamsCount: 0,
         playersCount: 0,
         matchesCount: 0,
         leaguesCount: 0,
+        seasonsCount: 0,
     });
 
     useEffect(() => {
         const fetchStats = async () => {
             try {
                 const response = await getAllStats()
-    
+
                 setStats({
                     teamsCount: response.teamsCount,
                     playersCount: response.playersCount,
-                    leaguesCount: response.leaguesCount
-                })    
+                    leaguesCount: response.leaguesCount,
+                    seasonsCount: response.seasonsCount
+                })
             } catch (error) {
-                console.log(error)
+                console.error(error)
             }
         }
-
-        console.log(stats)
 
         fetchStats()
     }, [])
@@ -34,40 +34,49 @@ export function AdminStats () {
         <>
             <div className="container-all-stats">
                 <div className="container-stats">
-                <div className="container-stats-image">
-                    <img src="team-color.png"></img>
-                </div>
-                <div className="container-stats-stats">
-                    <strong><p>{stats.teamsCount}</p></strong>
-                    <p>отбори</p>
-                </div>
-                </div>
-                <div className="container-stats">
-                <div className="container-stats-image">
-                    <img src="football-player.png"></img>
-                </div>
-                <div className="container-stats-stats">
-                    <strong><p>{stats.playersCount}</p></strong>
-                    <p>играчи</p>
-                </div>
-                </div>            
-                <div className="container-stats">
-                <div className="container-stats-image">
-                    <img src="football.png"></img>
-                </div>
-                <div className="container-stats-stats">
-                    <strong><p>9999</p></strong>
-                    <p>мачове</p>
-                </div>
+                    <div className="container-stats-image">
+                        <img src="team-color.png"></img>
+                    </div>
+                    <div className="container-stats-stats">
+                        <strong><p>{stats.teamsCount}</p></strong>
+                        <p>отбора</p>
+                    </div>
                 </div>
                 <div className="container-stats">
-                <div className="container-stats-image">
-                    <img src="leagues.png"></img>
+                    <div className="container-stats-image">
+                        <img src="football-player.png"></img>
+                    </div>
+                    <div className="container-stats-stats">
+                        <strong><p>{stats.playersCount}</p></strong>
+                        <p>играча</p>
+                    </div>
                 </div>
-                <div className="container-stats-stats">
-                    <strong><p>{stats.leaguesCount}</p></strong>
-                    <p>лиги</p>
+                <div className="container-stats">
+                    <div className="container-stats-image">
+                        <img src="football.png"></img>
+                    </div>
+                    <div className="container-stats-stats">
+                        <strong><p>9999</p></strong>
+                        <p>мача</p>
+                    </div>
                 </div>
+                <div className="container-stats">
+                    <div className="container-stats-image">
+                        <img src="leagues.png"></img>
+                    </div>
+                    <div className="container-stats-stats">
+                        <strong><p>{stats.leaguesCount}</p></strong>
+                        <p>лиги</p>
+                    </div>
+                </div>
+                <div className="container-stats">
+                    <div className="container-stats-image">
+                        <img src="season.png"></img>
+                    </div>
+                    <div className="container-stats-stats">
+                        <strong><p>{stats.seasonsCount}</p></strong>
+                        <p>сезона</p>
+                    </div>
                 </div>
             </div>
         </>
