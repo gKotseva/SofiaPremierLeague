ba2b004f150c41edde52bc161b6ebb780349fda9
Gabriela Kotseva <gkotseva98@gmail.com>
2024-07-12 20:13:58 +0300

    New form selector

--- client/src/components/admin/AdminHomeFormSelector.jsx
+++ client/src/components/admin/AdminHomeFormSelector.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './Admin.modules.css';
 
 export function FormSelector({ handleFormSelection }) {
-    const options = [
+    const optionsLists = [
         { bg: "играч", en: "players" },
         { bg: "отбор", en: "teams" },
         { bg: "мениджър", en: "managers" },
@@ -11,12 +11,23 @@ export function FormSelector({ handleFormSelection }) {
         { bg: "сезон", en: "seasons" },
     ];
 
+    const optionsAwards = [
+        {award: 'SELECT'},
+        {award: 'ARABESK'},
+        {award: 'KERELSKI'},
+        {award: 'GRIPSOCKS'},
+        {award: 'VR7 SERVICES'},
+        {award: 'Barber Shop Marty'},
+        {award: 'CAIRO'},
+    ];
+
     return (
+        <div className='main-dropdown-container'>
         <div className="dropdown-container">
-            <h3 className="dropdown-name">ДОБАВИ</h3>
+            <h3 className="dropdown-name">ДОБАВИ В СПИСЪЦИ</h3>
             <div className="dropdown-options">
                 <ul>
-                    {options.map((option, index) => (
+                    {optionsLists.map((option, index) => (
                         <li key={index} onClick={() => handleFormSelection(option.en)}>
                             {option.bg}
                         </li>
@@ -24,5 +35,18 @@ export function FormSelector({ handleFormSelection }) {
                 </ul>
             </div>
         </div>
+        <div className="dropdown-container">
+                <h3 className="dropdown-name">ДОБАВИ В НАГРАДИ</h3>
+                <div className="dropdown-options">
+                    <ul>
+                        {optionsAwards.map((option, index) => (
+                            <li key={index} onClick={() => handleFormSelection(option.en)}>
+                                {option.bg}
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+            </div>
+            </div>
     );
 }
