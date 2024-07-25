94af45c7adf5445998b78adfe93918e37f264549
Gabriela Kotseva <gkotseva98@gmail.com>
2024-07-25 18:04:21 +0300

    Fixed css classes

--- client/src/components/home/HomeImages/HomeImages.jsx
+++ client/src/components/home/HomeImages/HomeImages.jsx
@@ -4,9 +4,9 @@ export function Home () {
     return (
         <>
             <div className='container'>
-                <img src='gallery1.jpg'></img>
-                <img src='gallery2.jpg'></img>
-                <img src='gallery3.jpg'></img>
+                <img src='gallery1.jpg' className='homeImage'></img>
+                <img src='gallery2.jpg' className='homeImage'></img>
+                <img src='gallery3.jpg' className='homeImage'></img>
             </div>
         </>
     )
