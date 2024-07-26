12e6ba08eedc2d701b444f78789d1ccaeb72c3f1
Gabriela Kotseva <gkotseva98@gmail.com>
2024-07-26 22:43:54 +0300

    Date helper created

---
+++ client/src/utils/date.js
@@ -0,0 +1,6 @@
+const date = new Date()
+
+export const getYear = () => date.getFullYear()
+export const getDate = () => date.getDate()
+export const getDay = () => date.getDay()
+export const getMonth = () => date.getMonth()
