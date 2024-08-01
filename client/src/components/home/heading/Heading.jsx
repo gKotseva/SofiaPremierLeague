d68a700053908a14877f964c58484afe74853666
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-01 16:37:44 +0300

    New component created

---
+++ client/src/components/home/heading/Heading.jsx
@@ -0,0 +1,11 @@
+import './Heading.modules.css'
+
+export function Heading(){
+    return (
+        <div className="images-container">
+            <img src="gallery3.jpg" />
+            <img src="gallery1.jpg" />
+            <img src="gallery2.jpg" />
+      </div>
+    )
+}
