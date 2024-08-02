de42f83f458955a59fc0d3d23d41d5900cb86b01
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-02 20:22:40 +0300

    Service + controller added for new component

--- client/src/services/service.js
+++ client/src/services/service.js
@@ -2,6 +2,10 @@ import * as request from '../lib/request';
 
 export const getPenalties = async () => {
     const result = await request.get('/api/nakazaniq');
-    console.log(result)
+    return result;
+}
+
+export const getPlayersRightsRevoked = async () => {
+    const result = await request.get('/api/prava')
     return result;
 }
--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -12,4 +12,16 @@ router.get('/nakazaniq', async (req, res) => {
     }
   });
 
+
+router.get('/prava', async (req, res) => {
+  try {
+    const sqlQuery = `select * from players where rights_revoked = 1;`
+    const result = await db.executeQuery(sqlQuery)
+    res.json(result)
+  } catch (error) {
+    console.error('Error executing query:', error);
+    res.status(500).json({ error: 'Internal server error' });
+  }
+})
+
 module.exports = router;
