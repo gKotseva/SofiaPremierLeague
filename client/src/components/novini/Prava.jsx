a87232289337f9f6daf3bf094590caae77aa494c
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-02 20:22:26 +0300

    New component + styling created

--- client/src/components/novini/Prava.jsx
+++ client/src/components/novini/Prava.jsx
@@ -1,13 +1,31 @@
+import { useEffect, useState } from "react"
+import { getPlayersRightsRevoked } from "../../services/service"
+
 export function Prava(){
+    const [players, setPlayers] = useState([])
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const response = await getPlayersRightsRevoked()
+            setPlayers(response)
+        }
+
+        fetchData()
+    }, [])
+
+
     return (
         <>
             <div className="forbidden">
                 <h1>Играчи със спрени състезателни права</h1>
             </div>
             <div className="forbidden-players">
-                <p>Играч 1</p>
-                <p>Играч 2</p>
-                <p>Играч 3</p>
+                {players.map((item) => (
+                    <>
+                        <p key={item.player_id}>{item.name}</p>
+                        <hr />
+                    </>
+                ))}
             </div>
         </>
     )
