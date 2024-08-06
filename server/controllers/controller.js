d5c1754cf68bf9febdfc9b42eb64d320fc729a1e
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-06 22:32:52 +0300

    new controller for getting lineups created

--- server/controllers/controller.js
+++ server/controllers/controller.js
@@ -24,4 +24,61 @@ router.get('/prava', async (req, res) => {
   }
 })
 
+router.get('/lineups', async (req, res) => {
+
+    const superleague = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=8;
+`;
+    const malusSportLeague = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=1;
+`;
+    const cairoLeague = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=2;
+`;
+    const flameLeague = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=3;
+`;
+    const arabeskLeague = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=22;
+`;
+    const spl4League = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=44;
+`;
+    const spl5League = `
+    select * from player_stats_per_match pm
+join matches m on pm.match_id = m.match_id
+where season_id=8 and league_id=47;
+`;
+
+
+    try {
+        const [superLeague, malus, cairo, flame, arabesk, spl4, spl5] = await Promise.all([
+            db.executeQuery(superleague),
+            db.executeQuery(malusSportLeague),
+            db.executeQuery(cairoLeague),
+            db.executeQuery(flameLeague),
+            db.executeQuery(arabeskLeague),
+            db.executeQuery(spl4League),
+            db.executeQuery(spl5League),
+          ]);
+
+          res.send({superLeague, malus, cairo, flame, arabesk, spl4, spl5})
+    } catch (error) {
+      console.error('Error executing query:', error);
+      res.status(500).json({ error: 'Internal server error' });
+    }
+  })
+
 module.exports = router;
