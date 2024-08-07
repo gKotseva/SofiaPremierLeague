6f227a9005e2737c3bf10e22de4d5c9f5cc1395c
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-07 16:55:21 +0300

    New service added

--- client/src/services/lineupsService.js
+++ client/src/services/lineupsService.js
@@ -5,3 +5,8 @@ export const getLineups = async () => {
     const result = await request.get('/api/lineups');
     return result;
 };
+
+export const getStats = async () => {
+    const result = await request.get('/api/stats')
+    return result;
+}
