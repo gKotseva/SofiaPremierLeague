9ba099d8ad1390569f54ba4ec6b4455e43470370
Gabriela Kotseva <gkotseva98@gmail.com>
2024-08-09 16:29:08 +0300

    New component created

---
+++ client/src/components/loader/Loader.jsx
@@ -0,0 +1,13 @@
+import './Loader.modules.css'
+
+export function Loader() {
+    return (
+        <>
+        <div className='loading-container'>
+            <div className="loading">
+                <img src="football.gif" />
+            </div>
+        </div>
+        </>
+    )
+}
